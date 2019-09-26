6a40e34a4b99eae3e81f56203656a1eb7cd735bd
Aditya Kumar Iyer <aditya.kumar.iyer@gmail.com>
2019-09-26 12:15:35 +0530

    added one more

---
+++ scrape_bnk48/scrape_bnk48.py
@@ -0,0 +1,26 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import urllib
+
+class ScrapeBnk48:
+    url = "https://www.bnk48.com/index.php?page=members"
+    
+    def scrape(self):
+        fp = urllib.request.urlopen(self.url)
+        html = fp.read()
+        page = BeautifulSoup(html, "html.parser")
+        for div in page.findChildren("div", {"class": "boxMemx"}):
+            imgMemDiv = div.findChildren("div", {"class": "ImgMem"})[0]
+            tmp = imgMemDiv["style"]
+            index = tmp.find("(")
+            actualUrl = tmp[index+1: len(tmp) - 2]
+            actualUrl = "https://www.bnk48.com/" + actualUrl
+            nameMemDiv = div.findChildren("div", {"class": "nameMem"})[0]
+            fileName = nameMemDiv.text.strip() + ".png"
+            fileName = fileName
+            print(fileName)
+            urllib.request.urlretrieve(actualUrl, fileName)
+
+if __name__ == '__main__':
+    scraper = ScrapeBnk48()
+    scraper.scrape()
